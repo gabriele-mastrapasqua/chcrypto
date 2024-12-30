2acfa12669d0e71dcb51bfd166f0209d6c66cf07
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2024-12-30 17:51:28 +0100

    fix some issues with ch init scripts for schema migrations

--- clickhouse/init-clickhouse.sh
+++ clickhouse/init-clickhouse.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Start ClickHouse server in the background
-/entrypoint.sh clickhouse-server &
-
-# Wait for ClickHouse server to be ready
-until clickhouse-client -q "SELECT 1" &>/dev/null; do
-  echo "Waiting for ClickHouse to be ready..."
-  sleep 1
-done
+echo 'Waiting for ClickHouse to be ready...';
+sleep 5;
 
 # Run your initialization commands here
-clickhouse-client -q "CREATE DATABASE IF NOT EXISTS default"
-#clickhouse-client -q "CREATE TABLE IF NOT EXISTS default.my_table (id UInt32, name String) ENGINE = MergeTree() ORDER BY id"
+clickhouse-client --host clickhouse -q "CREATE DATABASE IF NOT EXISTS default"
 
 # Execute all SQL files in the ./clickhouse/schema directory
 #for sql_file in ./clickhouse/schema/*.sql; do
-for sql_file in /docker-entrypoint-initdb.d/*.sql; do
+for sql_file in /docker-entrypoint-initdb.d/schema/*.sql; do
   echo "Executing $sql_file..."
   #clickhouse-client -q "$(cat $sql_file)"
-  clickhouse-client -n < "$sql_file"
+  clickhouse-client --host clickhouse -n < "$sql_file"
 done
 
 echo "ClickHouse initialization completed."
