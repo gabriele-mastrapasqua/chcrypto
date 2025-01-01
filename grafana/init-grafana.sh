7a0b98dee483fc74da9da09715bca18d84aead09
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2025-01-01 16:42:18 +0100

    feat - fix grafana config + add default clickhouse syste analytics

--- grafana/init-grafana.sh
+++ grafana/init-grafana.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Wait for Grafana to be ready
-until curl -s http://localhost:3000/api/health | grep -q '\"database\":\"ok\"'; do
-  echo "Waiting for Grafana to be ready..."
-  sleep 1
-done
+# until curl -s http://localhost:3000/api/health | grep -q '\"database\":\"ok\"'; do
+#   echo "Waiting for Grafana to be ready..."
+#   sleep 1
+# done
+sleep 5
 
 # Provision the data source
-curl -X POST -H "Content-Type: application/json" -d @./grafana/datasource.yaml http://admin:admin@localhost:3000/api/datasources
+curl -X POST -H "Content-Type: application/json" -d @/etc/grafana/provisioning/datasources/datasource.yaml http://admin:admin@localhost:3000/api/datasources
