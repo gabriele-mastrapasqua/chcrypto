4c770a83418f0e87e0ad8316f2ea35c3fbd8e424
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2025-01-01 10:35:28 +0100

    wip file loader

--- ingest/loadTransactionsFile.ts
+++ ingest/loadTransactionsFile.ts
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import csv from 'csv-parser';
-import { z } from 'zod';
-import { Kafka } from 'kafkajs';
+import fs from "fs";
+import csv from "csv-parser";
+import { z } from "zod";
+import { Kafka } from "kafkajs";
 
 // Define the schema using zod
 const transactionSchema = z.object({
@@ -20,41 +20,47 @@ const transactionSchema = z.object({
 type Transaction = z.infer<typeof transactionSchema>;
 
 // Initialize Kafka
-const kafkaBroker = process.env.KAFKA_BROKER || 'localhost:9092';
+const kafkaBroker = process.env.KAFKA_BROKER || "localhost:9092";
 
 const kafka = new Kafka({
-  clientId: 'ch-crypto-transactions',
+  clientId: "ch-crypto-transactions",
   brokers: [kafkaBroker],
 });
 
 const producer = kafka.producer();
 
 async function run() {
-  await producer.connect();
+  // await for kafka client to connect
+  try {
+    await producer.connect();
+    console.log("Producer connected to Kafka");
+  } catch (error) {
+    console.error("Failed to connect to Kafka:", error);
+    process.exit(1); // Exit the process if unable to connect
+  }
 
   const results: Transaction[] = [];
 
-  fs.createReadStream('data/43114_txs.csv')
+  fs.createReadStream("data/43114_txs.csv")
     .pipe(csv())
-    .on('data', (data) => {
+    .on("data", (data) => {
       try {
         const transaction = transactionSchema.parse(data);
         results.push(transaction);
       } catch (e) {
-        console.error('Invalid data:', e.errors);
+        console.error("Invalid data:", e.errors);
       }
     })
-    .on('end', async () => {
+    .on("end", async () => {
+      // send all rows from file in Kafka
       for (const transaction of results) {
         await producer.send({
-          topic: 'transactions',
-          messages: [
-            { value: JSON.stringify(transaction) },
-          ],
+          topic: "ch-crypto-transactions",
+          messages: [{ value: JSON.stringify(transaction) }],
         });
       }
       await producer.disconnect();
-      console.log('All transactions have been sent to Kafka.');
+      console.log("All transactions have been sent to Kafka.");
     });
 }
 
